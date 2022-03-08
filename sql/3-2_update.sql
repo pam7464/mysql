df2fa8ee8eac62b818926eb0e8ad35dfbd75cb73
pam7464 <74638378+pam7464@users.noreply.github.com>
2022-03-08 17:58:07 +0900

    Add files via upload

---
+++ sql/3-2_update.sql
@@ -0,0 +1,8 @@
+/*
+을지문덕적의 publisher와 price를 '대한미디어',16500로 업데이트 하시오
+*/
+-- UPDATE book
+-- SET publisher='대한미디어',price=16500
+-- WHERE bookname='을지문덕전';
+
+SELECT * FROM book;
---
+++ sql/3-3_delete.sql
@@ -0,0 +1,6 @@
+SELECT * FROM book;
+
+DELETE FROM book
+WHERE publisher='대한미디어';
+
+SELECT * FROM book;
