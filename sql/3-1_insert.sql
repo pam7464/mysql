62ffe1bc16e373132f1793eb735c82ce69a49fa3
pam7464 <74638378+pam7464@users.noreply.github.com>
2022-03-08 17:37:05 +0900

    Add files via upload

---
+++ sql/3-1_insert.sql
@@ -0,0 +1,26 @@
+SELECT * FROM book;
+/*
+	11,'홍길동전','연희출판사',20000을  
+	book테이블의 bookid,bookname,publisher,price에 삽입
+*/
+-- INSERT INTO book(bookid,bookname,publisher,price) 
+-- VALUES(11,'홍길동전','연희출판사',20000);
+
+-- INSERT INTO book 
+-- VALUES(12,'성춘향전','연희출판사',18000);
+
+-- INSERT INTO book(bookname,bookid,price,publisher)
+-- VALUES('강감찬전',13,16000,'거기출판사');
+
+-- INSERT INTO book
+-- VALUES(14,'을지문덕전',NULL,NULL);
+
+SELECT * FROM book;
+
+
+
+
+
+
+
+
